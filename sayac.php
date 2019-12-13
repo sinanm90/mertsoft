6b2b8994e4097afdbea5c3ef49c8a149165caf9b
sinanm90 <57984866+sinanm90@users.noreply.github.com>
2019-12-13 14:16:51 +0300

    Create sayac.php

---
+++ sayac.php
@@ -0,0 +1,15 @@
+    <?php
+    $myFile = "say.txt";
+    $fh = fopen($myFile, 'r');
+    $theData = fread($fh, filesize($myFile));
+    fclose($fh);
+
+    $theData = $theData + 1;
+
+    $myFile = "say.txt";
+    $fh = fopen($myFile, 'w') or die("can't open file");
+    fwrite($fh, $theData);
+    fclose($fh);
+
+    header("Location:https://drive.google.com/uc?export=download&confirm=V48W&id=1ZLlyGUG9aznopQPWHHcpf19hK3KSaQkL");
+    ?>
